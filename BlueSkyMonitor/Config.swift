e16f09f30d18e944e66a2fb2f16abb5afe161bd7
ocskty <ocskty@ocsktyui-Macmini.local>
2026-02-26 11:48:19 +0900

    Set dev API base URL

--- BlueSkyMonitor/Config.swift
+++ BlueSkyMonitor/Config.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 struct AppConfig {
-    static var baseURL = URL(string: "https://your-api.domain")!
-    static var token: String? = nil
+    static var baseURL = URL(string: "http://192.168.0.210:3000")!
 }
